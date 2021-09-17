eac17bfe14bbd75aad857ae5cbcba33af7c871b1
ATuan <4A8K0069@stust.edu.tw>
2021-09-17 18:40:03 +0800

    Add Actor.cs and movement and viewAngle

---
+++ Assets/Script/Main/FPSMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Script{
+	public class FPSMovement : MonoBehaviour{
+
+		public Vector3 SelfPosition{
+			get => transform.position;
+			set => transform.position = value;
+		}
+
+		public void Move(Vector3 targetPosition){
+			SelfPosition += targetPosition;
+		}
+	}
+}
