23bb18f9b44e6e4b30f8143f15fa440769cb6839
ATuan <4A8K0069@stust.edu.tw>
2021-09-18 16:34:51 +0800

    complete player movement

--- Assets/Script/Main/FPSMovement.cs
+++ Assets/Script/Main/FPSMovement.cs
@@ -2,16 +2,21 @@
 
 namespace Script{
 	public class FPSMovement : MonoBehaviour{
-
-		public Vector3 SelfPosition => transform.position;
+		public Transform SelfPosition => transform;
 
 		private Rigidbody movementRigidbody;
+
 		private void Start(){
 			movementRigidbody = GetComponent<Rigidbody>();
 		}
 
 		public void Move(Vector3 targetPosition){
-			movementRigidbody?.AddForce(targetPosition , ForceMode.VelocityChange);
+			var moveDirection = targetPosition.normalized;
+			var moveWorldDirection = SelfPosition.TransformDirection(moveDirection) * 5f;
+			var movementVelocity = movementRigidbody.velocity;
+			var moveForce = new Vector3(moveWorldDirection.x - movementVelocity.x, 0f,
+				moveWorldDirection.z - movementVelocity.z);
+			movementRigidbody.AddForce(moveForce, ForceMode.VelocityChange);
 		}
 	}
 }
