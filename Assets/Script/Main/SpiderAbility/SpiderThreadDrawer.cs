c0de0eb53d09434d4dca4cabac29dcf550d25aaf
ATuan <4A8K0069@stust.edu.tw>
2021-09-23 14:40:16 +0800

    feat : createSpiderThread

---
+++ Assets/Script/Main/SpiderAbility/SpiderThreadDrawer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Script.Main.SpiderAbility{
+	public class SpiderThreadDrawer : MonoBehaviour{
+		private LineRenderer lineRenderer;
+		private void Start(){
+			lineRenderer = GetComponent<LineRenderer>();
+			EventBus.Subscribe<RaycastData>(DrawSpiderThread);
+		}
+
+		private void DrawSpiderThread(RaycastData obj){
+			var vertexPosition = new[]{ obj.OriginPosition, obj.AttachPosition };
+			lineRenderer.SetPositions(vertexPosition);
+		}
+		
+		
+		
+	}
+}
