5c648e7b99ced44b91aced4abcc6111b2c46f3eb
ATuan <4A8K0069@stust.edu.tw>
2021-09-30 23:25:48 +0800

    add feat ActorMovement.cs and try to solve spider force problem

--- Assets/Script/Main/Actor/ActorMovement.cs
+++ Assets/Script/Main/Actor/ActorMovement.cs
@@ -2,7 +2,17 @@
 
 namespace Script.Main{
 	public class ActorMovement : MonoBehaviour{
-		public Vector3 SelfPosition => transform.position;
+
+		public float Velocity{
+			get{
+				var rigidbodyVelocity = movementRigidbody.velocity;
+				var velocityMagnitude = rigidbodyVelocity.magnitude;
+				return velocityMagnitude;
+			}
+
+		}
+
+		public double Mass => movementRigidbody.mass;
 
 		private Rigidbody movementRigidbody;
 
@@ -17,12 +27,16 @@ namespace Script.Main{
 			var moveForce = new Vector3(moveWorldDirection.x - movementVelocity.x, 0f,
 				moveWorldDirection.z - movementVelocity.z);
 			movementRigidbody.AddForce(moveForce, ForceMode.VelocityChange);
-			movementRigidbody.AddForce(Physics.gravity, ForceMode.Acceleration);
 		}
 
 		public void JumpBehavior(float jumpForce){
 			movementRigidbody.AddForce(Vector3.up * jumpForce , ForceMode.Impulse);
-			movementRigidbody.AddForce(Physics.gravity, ForceMode.Acceleration);
+		}
+
+		public void AddForce(Vector3 direction, float forceValue){
+			var force = direction * forceValue;
+			Debug.Log($"force = {force}");
+			movementRigidbody.AddForce(force , ForceMode.Impulse);
 		}
 	}
 }
--- Assets/Script/Main/SpiderAbility/SpiderThreadBehavior.cs
+++ Assets/Script/Main/SpiderAbility/SpiderThreadBehavior.cs
@@ -1,23 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Script.Main.SpiderAbility{
 	public class SpiderThreadBehavior : MonoBehaviour{
 		private Vector3 threadAttachPosition;
-		private Vector3 threadOriginPosition;
 		private SpiderTreadDetector spiderTreadDetector;
 		private ActorMovement actorMovement;
 		
 		private void Start(){
 			actorMovement = GetComponentInParent<ActorMovement>();
 			spiderTreadDetector = GetComponent<SpiderTreadDetector>();
-			EventBus.Subscribe<SpiderTreadCreated>(OnTreadCreated);
+			EventBus.Subscribe<SpiderTreadCreated>(StartSwingBehavior);
 		}
 
 		private void Update(){
-			if(Input.GetMouseButton(0)){
-				FixThreadPoint();
-			}
-
 			if(Input.GetMouseButtonDown(0)){
 				spiderTreadDetector.CreateTreadData(transform.forward);
 			}
@@ -25,19 +21,25 @@ namespace Script.Main.SpiderAbility{
 			if(Input.GetMouseButtonUp(0)){
 				StopSwingBehavior();
 			}
+
+			if(Input.GetMouseButton(0)){
+				FixThreadPoint();
+			}
 		}
 
-		private void OnTreadCreated(SpiderTreadCreated obj){
+		private void StartSwingBehavior(SpiderTreadCreated obj){
+			// Tension = L(((m(v*v))/r)) + (mg* sin * angle)
 			threadAttachPosition = obj.AttachPosition;
-			threadOriginPosition = obj.OriginPosition;
-			StartSwingBehavior(threadAttachPosition, obj.Distance);
+			var originPosition = obj.OriginPosition;
+			var objDistance = obj.Distance;
+			var angle = Vector3.Angle(originPosition, threadAttachPosition);
+			var velocity = actorMovement.Velocity;
+			actorMovement.AddForce(transform.forward + Vector3.up , objDistance);
 		}
 
 		private void StopSwingBehavior(){
 		}
-
-		private void StartSwingBehavior(Vector3 attachPosition, float distance){
-		}
+		
 
 		private void FixThreadPoint(){
 			var position = transform.position;
