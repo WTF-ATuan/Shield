d18d0adc28cfca202a725d65447a7697d47aa7f4
ATuan <4A8K0069@stust.edu.tw>
2021-12-28 00:31:57 +0800

    製作 AggregateRoot.cs 存放 event 以及隱藏技術細節

---
+++ Assets/Script/Main/Utility/Base/AggregateRoot.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Script.Main.Utility.Base{
+	public abstract class AggregateRoot : IAggregateRoot{
+		private readonly Dictionary<Type, List<CustomEvent>> _eventBuffer = new Dictionary<Type, List<CustomEvent>>();
+
+		protected void SaveEvent<T>(T eventData) where T : CustomEvent{
+			var eventType = typeof(T);
+			var containsKey = _eventBuffer.ContainsKey(eventType);
+			if(containsKey){
+				_eventBuffer[eventType].Add(eventData);
+			}
+			else{
+				var customEvents = new List<CustomEvent>{ eventData };
+				_eventBuffer.Add(eventType, customEvents);
+			}
+		}
+
+		public List<T> GetEvent<T>() where T : CustomEvent{
+			var eventType = typeof(T);
+			var containsKey = _eventBuffer.ContainsKey(eventType);
+			if(containsKey){
+				var customEvents = _eventBuffer[eventType];
+				var typeEvents = customEvents.Cast<T>().ToList();
+				return typeEvents;
+			}
+
+			var events = new List<T>();
+			return events;
+		}
+	}
+
+	public interface IAggregateRoot{
+		List<T> GetEvent<T>() where T : CustomEvent;
+	}
+}
