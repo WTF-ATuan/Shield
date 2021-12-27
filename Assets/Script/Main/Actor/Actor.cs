0dae324b97be55669c1dd60cd82aafd71a37a6bb
ATuan <4A8K0069@stust.edu.tw>
2021-12-28 00:26:48 +0800

    傳送事件 以及事件實作

--- Assets/Script/Main/Actor/Actor.cs
+++ Assets/Script/Main/Actor/Actor.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Script.Main.Actor.Event;
 using Script.Main.Utility;
 using UnityEngine;
 
 namespace Script.Main.Actor{
 	public class Actor{
-		private List<CustomEvent> eventBufferList;
+		private readonly Dictionary<Type, List<CustomEvent>> _eventBuffer = new Dictionary<Type, List<CustomEvent>>();
 
-		private void SaveEvent<T>(T eventData) where T : CustomEvent{ }
+		private void SaveEvent<T>(T eventData) where T : CustomEvent{
+			var eventType = typeof(T);
+			var containsKey = _eventBuffer.ContainsKey(eventType);
+			if(containsKey){
+				_eventBuffer[eventType].Add(eventData);
+			}
+			else{
+				var customEvents = new List<CustomEvent>{ eventData };
+				_eventBuffer.Add(eventType, customEvents);
+			}
+		}
 
 		public List<T> GetEvent<T>() where T : CustomEvent{
-			return null;
+			var eventType = typeof(T);
+			var containsKey = _eventBuffer.ContainsKey(eventType);
+			if(containsKey){
+				var customEvents = _eventBuffer[eventType];
+				var typeEvents = customEvents.Cast<T>().ToList();
+				return typeEvents;
+			}
+			var events = new List<T>();
+			return events;
 		}
-		
 
-		public void Move(Vector3 direction){ }
+
+		public void Move(Vector3 direction){
+			var actorMoved = new ActorMoved(direction);
+			SaveEvent(actorMoved);
+		}
 	}
 }
--- Assets/Script/Main/Actor/Event/ActorMoved.cs
+++ Assets/Script/Main/Actor/Event/ActorMoved.cs
@@ -1,5 +1,12 @@
 ﻿using Script.Main.Utility;
+using UnityEngine;
 
 namespace Script.Main.Actor.Event{
-	public class ActorMoved : ViewEvent{ }
+	public class ActorMoved : ViewEvent{
+		public Vector3 Direction{ get; }
+
+		public ActorMoved(Vector3 direction){
+			Direction = direction;
+		}
+	}
 }
