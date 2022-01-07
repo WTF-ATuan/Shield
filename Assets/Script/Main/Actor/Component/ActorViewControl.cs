95466222feba170b0ea2491cf7df7b6450b1a271
ATuan <4A8K0069@stust.edu.tw>
2022-01-07 13:49:09 +0800

    view Controller

---
+++ Assets/Script/Main/Actor/Component/ActorViewControl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Script.Main.Actor.UseCase;
+using UnityEngine;
+
+namespace Script.Main.Actor.Component{
+	public class ActorViewControl : MonoBehaviour{
+		[SerializeField] private string actorID;
+
+		private readonly ActorUseCase _actorUseCase = new ActorUseCase();
+
+		private void Start(){
+			actorID = Guid.NewGuid().ToString();
+			_actorUseCase.CreateActor(actorID);
+		}
+
+		private void Update(){
+			var horizontalValue = Input.GetAxisRaw("Horizontal");
+			var verticalValue = Input.GetAxisRaw("Vertical");
+			var moveValue = new Vector3(horizontalValue, 0, verticalValue);
+			_actorUseCase.MoveActor(actorID, moveValue);
+		}
+	}
+}
---
+++ Assets/Script/Main/Actor/Component/ActorViewEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Script.Main.Actor.Event;
+using Script.Main.Utility;
+using UnityEngine;
+
+namespace Script.Main.Actor.Component{
+	public class ActorViewEventHandler : MonoBehaviour{
+		private Rigidbody _rigidbody;
+
+		private void Start(){
+			_rigidbody = GetComponent<Rigidbody>();
+			EventBus.Subscribe<ViewEvent>(ViewEventHandler);
+		}
+
+		private void ViewEventHandler(ViewEvent obj){
+			var type = obj.GetType();
+			if(type == typeof(ActorMoved)){
+				var actorMoved = (ActorMoved)obj;
+				OnActorMoved(actorMoved);
+			}
+		}
+
+		private void OnActorMoved(ActorMoved obj){
+			var direction = obj.Direction;
+			_rigidbody.velocity = direction;
+		}
+	}
+}
