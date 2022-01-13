c1cfb5abc4609d6cbd2f5e73baf6d9e9c4c8e06e
ATuan <4A8K0069@stust.edu.tw>
2022-01-13 16:50:16 +0800

    Create ActorController.cs

---
+++ Assets/Script/Main/Actor/Component/ActorController.cs
@@ -0,0 +1,31 @@
+﻿using Script.Main.Actor.Repository;
+using Script.Main.Actor.UseCase;
+using UnityEngine;
+
+namespace Script.Main.Actor.Component{
+	public class ActorController : MonoBehaviour{
+		private ActorRepository _actorRepository;
+		private ActorUseCase _actorUseCase;
+
+		private void Awake(){
+			_actorRepository = new ActorRepository();
+			_actorUseCase = new ActorUseCase(_actorRepository);
+		}
+
+		public void CreateActor(string uid, int defaultHealth){
+			_actorUseCase.CreateActor(uid, defaultHealth);
+		}
+
+		public void MoveActor(string uid, Vector3 inputValue){
+			_actorUseCase.MoveActor(uid, inputValue);
+		}
+
+		public void EquipWeapon(string uid, Weapon.Entity.Weapon weapon){
+			_actorUseCase.EquipWeapon(uid, weapon);
+		}
+
+		public void ActorFire(string uid){
+			_actorUseCase.MakeActorFire(uid);
+		}
+	}
+}
