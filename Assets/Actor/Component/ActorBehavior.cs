bdf132971fc4601b1d25c1370732f9a9cfe9d42f
ATuan <4A8K0069@stust.edu.tw>
2022-01-15 13:53:12 +0800

    add EventBus.cs

--- Assets/Actor/Component/ActorBehavior.cs
+++ Assets/Actor/Component/ActorBehavior.cs
@@ -1,7 +1,26 @@
 using UnityEngine;
 
 namespace Actor.Component{
-	public class ActorBehavior : MonoBehaviour
-	{
+	public class ActorBehavior : MonoBehaviour{
+		[SerializeField] private float moveSpeed;
+		[SerializeField] private float jumpForce;
+		
+
+		private Rigidbody _rigidbody;
+
+		private void Start(){
+			_rigidbody = GetComponent<Rigidbody>();
+		}
+
+		public void MoveActor(Vector3 direction , bool isJump){
+			var currentVelocityOffsetY = _rigidbody.velocity.y;
+			var moveVelocity = new Vector3(direction.x , currentVelocityOffsetY , direction.z);
+			_rigidbody.velocity = moveVelocity;
+		}
+
+		public void SetFaceDirection(){
+			
+		}
 	}
+
 }
