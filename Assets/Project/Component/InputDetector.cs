1c56b122c686ab284bee480c388757a4b08ecc45
ATuan <4A8K0069@stust.edu.tw>
2022-01-15 15:28:29 +0800

    MouseInput

--- Assets/Project/Component/InputDetector.cs
+++ Assets/Project/Component/InputDetector.cs
@@ -5,15 +5,34 @@ namespace Project.Component{
 	public class InputDetector : MonoBehaviour{
 		private void Update(){
 			DetectMoveInput();
+			DetectRightMouseButton();
+			DetectLeftMouseButton();
+		}
+
+		private void DetectRightMouseButton(){
+			var rightMouseButtonDown = Input.GetMouseButtonDown(0);
+			var rightMouseButton = Input.GetMouseButton(0);
+			var rightMouseButtonUp = Input.GetMouseButtonUp(0);
+			var rightMouseInputDetected =
+					new MouseInputDetected(true, rightMouseButtonDown, rightMouseButton, rightMouseButtonUp);
+			EventBus.Post(rightMouseInputDetected);
+		}
+
+		private void DetectLeftMouseButton(){
+			var leftMouseButtonDown = Input.GetMouseButtonDown(1);
+			var leftMouseButton = Input.GetMouseButton(1);
+			var leftMouseButtonUp = Input.GetMouseButtonUp(1);
+			var leftMouseInputDetected =
+					new MouseInputDetected(false, leftMouseButtonDown, leftMouseButton, leftMouseButtonUp);
+			EventBus.Post(leftMouseInputDetected);
 		}
 
 		private void DetectMoveInput(){
 			var horizontalValue = Input.GetAxisRaw($"Horizontal");
 			var verticalValue = Input.GetAxisRaw($"Vertical");
 			var isJump = Input.GetKeyDown(KeyCode.Space);
-			var moveInputDetected = new MoveInputDetected(horizontalValue , verticalValue , isJump);
+			var moveInputDetected = new MoveInputDetected(horizontalValue, verticalValue, isJump);
 			EventBus.Post(moveInputDetected);
 		}
-		
 	}
 }
