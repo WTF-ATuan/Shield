8fc43245fed359d60756fc767f06de7729203c5f
ATuan <4A8K0069@stust.edu.tw>
2022-01-15 14:09:17 +0800

    InputEvent Fired

---
+++ Assets/Project/Component/InputDetector.cs
@@ -0,0 +1,19 @@
+﻿using Project.Event;
+using UnityEngine;
+
+namespace Project.Component{
+	public class InputDetector : MonoBehaviour{
+		private void Update(){
+			DetectMoveInput();
+		}
+
+		private void DetectMoveInput(){
+			var horizontalValue = Input.GetAxisRaw($"Horizontal");
+			var verticalValue = Input.GetAxisRaw($"Vertical");
+			var isJump = Input.GetKeyDown(KeyCode.Space);
+			var moveInputDetected = new MoveInputDetected(horizontalValue , verticalValue , isJump);
+			EventBus.Post(moveInputDetected);
+		}
+		
+	}
+}
---
+++ Assets/Project/Event/MoveInputDetected.cs
@@ -0,0 +1,13 @@
+﻿namespace Project.Event{
+	public class MoveInputDetected{
+		public float HorizontalValue{ get; }
+		public float VerticalValue{ get; }
+		public bool IsJump{ get; }
+
+		public MoveInputDetected(float horizontalValue ,float verticalValue, bool isJump){
+			HorizontalValue = horizontalValue;
+			VerticalValue = verticalValue;
+			IsJump = isJump;
+		}
+	}
+}
