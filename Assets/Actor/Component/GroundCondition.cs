e1ab7a0d6a9e12e33eff4c9ba3c34eb85cad7818
ATuan <4A8K0069@stust.edu.tw>
2022-01-20 13:28:57 +0800

    ground Condition Set

--- Assets/Actor/Component/GroundCondition.cs
+++ Assets/Actor/Component/GroundCondition.cs
@@ -6,17 +6,26 @@ using UnityEngine;
 
 namespace Actor.Component{
 	public class GroundCondition : MonoBehaviour, IActorMoveCondition{
+		[SerializeField] private float detectRange = 1.5f;
+		[SerializeField] private bool isGround;
+
+		private ActorMoveData _actorMoveData;
+
 		private void Start(){
 			var moveConditionAdded = new ActorMoveConditionAdded(this);
 			EventBus.Post(moveConditionAdded);
 		}
 
 		public void Condition(float horizontal, float vertical, bool isJump){
-			throw new System.NotImplementedException();
+			var actorTransform = transform;
+			isGround = Physics.Raycast(actorTransform.position, Vector3.down, detectRange);
+			_actorMoveData = isJump
+					? new ActorMoveData(horizontal, vertical, isGround)
+					: new ActorMoveData(horizontal, vertical, false);
 		}
 
 		public ActorMoveData GetConditionData(){
-			throw new System.NotImplementedException();
+			return _actorMoveData;
 		}
 	}
 }
